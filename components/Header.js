b89afd2ebab1943c73f8c30c8d14001dd290823e
Yhtyyar Annayev <70913322+yhtyyar94@users.noreply.github.com>
2022-10-10 15:14:45 +0200

    nextjs basics

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+import headerStyles from "../styles/Header.module.css";
+
+const Header = () => {
+  const x = 2;
+  return (
+    <div>
+      <h1 className={headerStyles.title}>
+        <span>WebDev</span>News
+      </h1>
+      <p className={headerStyles.description}>
+        Keep up to date with latest web dev news
+      </p>
+    </div>
+  );
+};
+
+export default Header;
---
+++ pages/api/articles/[id].js
@@ -0,0 +1,13 @@
+import { articles } from "../../../data";
+
+export default function handler(req, res) {
+  const { id } = req.query;
+  const filtered = articles.filter((article) => article.id === id);
+  if (filtered.length > 0) {
+    res.status(200).json(filtered[0]);
+  } else {
+    res
+      .status(404)
+      .json({ message: `Article with the id of ${id} is not found` });
+  }
+}
